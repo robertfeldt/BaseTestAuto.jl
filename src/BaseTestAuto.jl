a664ad5dcf12bccb9eaf95a9a9e5c7d33a0390e2
Robert Feldt <robert.feldt@gmail.com>
2016-03-28 11:27:06 +0200

    cleaner testset helper methods

--- src/BaseTestAuto.jl
+++ src/BaseTestAuto.jl
@@ -1,30 +1,39 @@
 """
-Alternative BaseTestNext implementation which copies most of it and then
-extends it to support repetition and automated test data generation:
-
-* `@multitest`
-* `@generate`
-
-The default, task-level test set will repeat the tests in a block if there
-are calls to the generate macro among their tests.
+BaseTestAuto extends the Base.Test implementation from Julia 0.5.0-dev
+with support for automated testing. It can be used as a drop-in replacement
+but with support for test data generation, repeated testing etc.
 """
 module BaseTestAuto
 
-# Dirty hack for now since I found no way to reuse most of it while still
-# redefining the testset_beginend and testset_forloop methods. TOFIX!
-include("parts_from_BaseTestNext.jl")
+#
+# We either use the orig part of base/test.jl unless we have extended it:
+#
 
-# Extend the base code with the minor changes needed to create the hooks we want
-# for more powerful automated testing. Over time we hope that these extensions
-# will be added to BaseTestNext and potentially Base.Test so we need not copy-paste
-# that code like we currently do.
-include("extensions_to_BaseTestNext.jl")
+include("orig_base_test_parts/test_result.jl"),
+
+include("orig_base_test_parts/test_macro.jl"),
+
+include("orig_base_test_parts/test_throws_macro.jl"),
+
+include("orig_base_test_parts/abstract_testset.jl"),
+
+include("orig_base_test_parts/fallback_testset.jl"),
+
+include("orig_base_test_parts/default_testset.jl"),
+
+include("orig_base_test_parts/testset_macros.jl"),
+
+#include("orig_base_test_parts/testset_helper_methods.jl"),
+include("base_test_changes/testset_helper_methods.jl"),
+
+include("orig_base_test_parts/legacy_testing_methods.jl")
+
+
+# Fully new stuff
+#include("extract_sub_expressions.jl")
 
 # Export the same as exported by BaseTestNext
 export @test, @test_throws, @testset
 export @test_approx_eq, @test_approx_eq_eps, @inferred
-
-# And now start adding our new stuff.
-include("multitest.jl")
 
 end # BaseTestAuto module
---
+++ src/base_test_changes/testset_helper_methods.jl
@@ -0,0 +1,56 @@
+# Various helper methods for test sets
+
+const TESTSET_STACK_KEY = :__BASETESTNEXT__
+
+function get_testset_stack()
+    get(task_local_storage(), TESTSET_STACK_KEY, AbstractTestSet[])
+end
+
+function set_testset_stack(testsets)
+    setindex!(task_local_storage(), testsets, TESTSET_STACK_KEY)
+end
+
+"""
+    get_testset()
+
+Retrieve the active test set from the task's local storage. If no
+test set is active, use the fallback default test set.
+"""
+function get_testset()
+    testsets = get_testset_stack()
+    return isempty(testsets) ? fallback_testset : testsets[end]
+end
+
+"""
+    push_testset(ts::AbstractTestSet)
+
+Adds the test set to the task_local_storage.
+"""
+function push_testset(ts::AbstractTestSet)
+    testsets = get_testset_stack()
+    push!(testsets, ts)
+    set_testset_stack(testsets)
+end
+
+"""
+    pop_testset()
+
+Pops the last test set added to the task_local_storage. If there are no
+active test sets, returns the fallback default test set.
+"""
+function pop_testset()
+    testsets = get_testset_stack()
+    ret = isempty(testsets) ? fallback_testset : pop!(testsets)
+    set_testset_stack(testsets)
+    return ret
+end
+
+"""
+    get_testset_depth()
+
+Returns the number of active test sets, not including the defaut test set
+"""
+function get_testset_depth()
+    testsets = get_testset_stack()
+    return length(testsets)
+end
